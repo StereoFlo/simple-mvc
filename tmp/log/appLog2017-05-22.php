d8ca45e662d92c073d2077004f842173f64c119a
Evgeniy <e.bronnikov@petrovich.ru>
2017-06-29 10:53:40 +0300

    gitignore fix

--- tmp/log/appLog2017-05-22.php
+++
@@ -1,2 +0,0 @@
-0: Too few arguments to function Controllers\Index::index(), 0 passed and exactly 2 expected
-0: Too few arguments to function Controllers\Index::index(), 0 passed and exactly 2 expected
--- tmp/log/appLog2017-05-23.php
+++
@@ -1,2 +0,0 @@
-0: Argument 1 passed to Core\ClassicRouter::init() must be of the type string, null given, called in /Users/evgen/Downloads/simple-mvc/src/Application.php on line 72
-0: Argument 1 passed to Core\ClassicRouter::init() must be of the type string, null given, called in /Users/evgen/Downloads/simple-mvc/src/Application.php on line 72
